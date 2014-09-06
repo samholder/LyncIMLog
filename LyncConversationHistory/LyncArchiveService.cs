579303de39836a9df3191f64248457453f703568
sam holder <samholder@hotmail.com>
2014-09-06 18:15:25 +0100

    Created template project

---
+++ LyncConversationHistory/LyncArchiveService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LyncConversationHistory
+{
+    public partial class LyncArchiveService : ServiceBase
+    {
+        public LyncArchiveService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        public void Start(string[] args)
+        {
+            OnStart(args);
+        }
+
+        public void DoStop()
+        {
+            OnStop();
+        }
+    }
+}
---
+++ LyncConversationHistory/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LyncConversationHistory
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            var ServiceToRun = new LyncArchiveService();
+            if (Environment.UserInteractive)
+            {
+                // This used to run the service as a console (development phase only)
+
+                ServiceToRun.Start(new []{@"c:\temp\LyncLogs\"});
+
+                Console.WriteLine("Press Enter to terminate ...");
+                Console.ReadLine();
+
+                ServiceToRun.DoStop();
+            }
+            else
+            {
+                ServiceBase.Run(ServiceToRun);
+            }
+        }
+    }
+}
